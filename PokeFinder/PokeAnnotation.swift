c9a9ddc78e1ec1fc426468aea5c73769954bb103
Vlad Krupenko <dev.fixique@gmail.com>
2017-03-22 22:55:29 +0300

    Next part of application. Work random set pokemon on the map and show user location

---
+++ PokeFinder/PokeAnnotation.swift
@@ -0,0 +1,9 @@
+//
+//  PokeAnnotation.swift
+//  PokeFinder
+//
+//  Created by Vlad Krupenko on 22.03.17.
+//  Copyright © 2017 fixique. All rights reserved.
+//
+
+import Foundation
---
+++ PokeFinder/PokeFinder-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  PokeFinder-Bridging-Header.h
+//  PokeFinder
+//
+//  Created by Vlad Krupenko on 22.03.17.
+//  Copyright © 2017 fixique. All rights reserved.
+//
+
+#ifndef PokeFinder_Bridging_Header_h
+#define PokeFinder_Bridging_Header_h
+
+
+#endif /* PokeFinder_Bridging_Header_h */
